9ef709bf39e6e021c743e0c0da5029b2ad029e3c
Pankaj Mahanta <Pankaj Mahanta@SCSDESK0004>
2019-07-31 14:39:06 +0530

    feat(demo-controller): add demo controller and view page

---
+++ GroupMeetingASP.NETCoreWebApp/Controllers/DemoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupMeetingASP.NETCoreWebApp.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Demo()
+        {
+            return View();
+        }
+
+
+    }
+}
